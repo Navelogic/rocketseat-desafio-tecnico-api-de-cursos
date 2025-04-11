333764b65c74e241774843c5bae632a901746280
Navelogic <weslleysantoszbd@gmail.com>
2025-04-11 15:39:13 -0300

    initialize project with Maven and Spring Boot setup

---
+++ src/main/java/com/github/navelogic/rdtac/RocketseatDesafioTecnicoApiDeCursosApplication.java
@@ -0,0 +1,13 @@
+package com.github.navelogic.rdtac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketseatDesafioTecnicoApiDeCursosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketseatDesafioTecnicoApiDeCursosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/navelogic/rdtac/RocketseatDesafioTecnicoApiDeCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.navelogic.rdtac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketseatDesafioTecnicoApiDeCursosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
